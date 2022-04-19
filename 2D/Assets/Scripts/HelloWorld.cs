6314e0cdb9f8943361413ec82a70d742866b3a06
Jose Ciccio <jciccio@gmail.com>
2022-04-18 20:26:35 -0600

    Ejemplo clase 4 agregado

--- 2D/Assets/Scripts/HelloWorld.cs
+++ 2D/Assets/Scripts/HelloWorld.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class HelloWorld : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello World: Start Function " + this.name);       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Hello World: Update Function "+ this.name);
     }
 }
