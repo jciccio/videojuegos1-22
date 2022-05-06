c7b1dbb7cff0e1e991a5123f545e6eb089e94ac0
Jose Ciccio <jciccio@gmail.com>
2022-05-06 07:30:45 -0600

    Ejemplo físicas y salto del jugador agregado (videos asincronos 1-4)

---
+++ 2D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float horizontalVelocity = 5f;
+    [SerializeField] float verticalVelocity = 20f;
+    [SerializeField] float forceX = 1000f;
+
+    private Rigidbody2D physics;
+    public bool canJump = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        physics = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.D)){
+            physics.AddForce(new Vector2(forceX * Time.deltaTime, 0));
+        }
+        if(Input.GetKey(KeyCode.A)){
+            physics.AddForce(new Vector2(-1* forceX * Time.deltaTime, 0));
+        }
+        
+    }
+
+    public void Jump(InputAction.CallbackContext context){
+        Debug.Log("Jump: " + context);
+        if(context.performed){
+            physics.AddForce((Vector2.up * 5f), ForceMode2D.Impulse);
+        }
+    }
+}
